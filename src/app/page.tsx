0221c7594202d492390b53cde1dfcf6e5481de36
Ali Raza <malik537ar@gmail.com>
2025-08-27 15:21:59 +0500

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useEffect, useState, useCallback } from "react";
 import { Chat, Message } from "@/types";
 import { ChatController } from "@/app/controllers/chatController";
